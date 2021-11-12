e8d9e58e058dc776eda011652bec6b603610893c
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-13 00:20:34 +0100

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-
+import About from './components/pages/About';
+import Home from './components/pages/Home';
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,12 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faIdCard } from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'react-router-dom';
+
 const Navbar = ({ title, icon }) => {
   return (
     <div className='navbar bg-primary'>
       <h1>
         <FontAwesomeIcon icon={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
